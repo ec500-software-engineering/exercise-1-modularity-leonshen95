e3da840762171067137badf324bd99e8f4e98a67
leonshen95 <leyang.shen@uconn.edu>
2019-02-17 17:44:54 -0500

    Update OutputAlert_module.py

--- OutputAlert_module.py
+++ OutputAlert_module.py
@@ -3,17 +3,17 @@ def receive_basic_iuput_data(Singal_Loss, Shock_Alert, Oxygen_Supply, Fever, Hyp
  ##Boolean Parameters
  ##If paramter returns True, means it should be alerted, then add it to the array
     BasicResult = {'Signal_Loss':False, 'Shock_Alert':False,'Oxygen_Supply':False,'Fever':False,'Hypotension':False,'Hypertension':False}
-    if(Singal_Loss == True):
+    if Singal_Loss:
         BasicResult['Signal Loss']=True
-    if(Shock_Alert == True):
+    if Shock_Alert:
         BasicResult['Shock_Alert']=True 
-    if(Oxygen_Supply == True):
+    if Oxygen_Supply:
         BasicResult['Oxygen_Supply']=True 
-    if(Fever == True):
+    if Fever:
         BasicResult['Fever']=True
-    if(Hypotension == True):
+    if Hypotension:
         BasicResult['Hypotension']=True
-    if(Hypertension == True):
+    if Hypertension:
         BasicResult['Hypertension']=True 
 
     return BasicResult
